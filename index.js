c387e9543642b9ab9db741983cd1023ec7eb617d
Bilal815 <62904483+Bilal815@users.noreply.github.com>
2020-06-22 13:38:25 +0500

    JavaScript Bootcamp

---
+++ index.js
@@ -0,0 +1,9 @@
+/* To check Node version: >node -v  OR >npode --version
+   To check npm version: >npm -v OR >npm --version
+   To clear the terminal: >clear
+   To install npm for surge globally(thorughout projects/vscode): >npm install -g surge OR >npm install --globally surge
+   To install npm for surge locally(for just one project/folder): >npm install surge
+   To get surge token(credential obtained once): >surge token
+   To deploy folder/project: >surge and type url name
+    
+*/
